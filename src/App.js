3faec5af3b098c1796f42293c7912d50a0cd997c
PAXANDDOS <40315177+PAXANDDOS@users.noreply.github.com>
2021-05-16 18:18:53 +0300

    Progress. Can't get it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import Header from './Header/Header.js';
-import Preview from './Header/Preview.js';
+import Header from './Headers/Header.js';
+import Preview from './Headers/Preview.js';
+import AllPosts from './Posts/All.js';
 
 function App() {
   return (
-    <>
+    <main>
       <Header />
       <Preview />
-    </>
+      <AllPosts />
+    </main>
   );
 }
 
