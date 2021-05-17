96746b49f77946d579f6e03457abbf3363431568
Paul <40315177+PAXANDDOS@users.noreply.github.com>
2021-05-17 14:06:02 +0300

    Optimized by @Savolus

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import Header from './Headers/Header.js';
 import Preview from './Headers/Preview.js';
 import AllPosts from './Posts/All.js';
 
 function App() {
   return (
-    <main>
+    <>
       <Header />
       <Preview />
       <AllPosts />
-    </main>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
